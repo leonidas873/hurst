31d0020e6941ca3597c3c90879279356dc4760a4
giobitsa <giorgi.bitsadze01@gmail.com>
2022-05-16 12:48:38 +0400

    added navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,73 +1,9 @@
 import React, { useState } from 'react';
-import { AppBar, Box, Toolbar, IconButton, Badge, Popover, List, ListItem, ListItemButton, ListItemText, Drawer, Typography } from '@mui/material'
-import { styled } from '@mui/system';
+import { Box, Toolbar, Badge, Popover, List, ListItemButton, Drawer, Typography } from '@mui/material'
+import { MyAppBar, Image, MyListItem, MyListItemText, AllProducts, SingleProduct, ImageContainer, ProductInfo, MyIconButton, CloseIconButton, Total, RightIconButton } from './Style';
 import { Link } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi';
 import { FaShoppingCart } from 'react-icons/fa'
 import { AiOutlineClose } from 'react-icons/ai'
-
-const Image = styled('img')({
-    display: 'block',
-    margin: '0 auto',
-});
-
-const MyAppBar = styled(AppBar)({
-    color: 'black',
-    backgroundColor: '#c5c5c5',
-    boxShadow: 'none',
-})
-
-const MyListItem = styled(ListItem)({
-    background: 'linear-gradient(to left, white 50%, #c8a165 50%) right',
-    backgroundSize: '200%',
-    transition: 'all .3s ease 0s',
-
-    '&:hover': {
-        backgroundPosition: 'left'
-    },
-
-    '&:hover .MuiTypography-root': {
-        color: 'white'
-    }
-})
-
-const MyListItemText = styled(ListItemText)({
-    '& .MuiTypography-root': {
-        textTransform: 'uppercase',
-        paddingLeft: '70px',
-        color: '#666',
-        display: 'block',
-        fontSize: '14px',
-        fontWeight: '700',
-        lineHeight: '45px',
-        position: 'relative',
-        zIndex: '1',
-    }
-})
-
-const AllProducts = styled('div')({
-    padding: '10px 0'
-})
-
-const SingleProduct = styled('div')({
-    padding: '10px 30px 10px 20px'
-})
-
-const ImageContainer = styled('div')({
-    float: 'left',
-    width: '90px',
-})
-
-const ProductInfo = styled('div')({
-    float: 'left',
-    paddingLeft: '20px',
-    textAlign: 'left',
-
-    '&: a': {
-        fontSize: '14px',
-        textTransform: 'uppercase',
-    }
-})
 
 const Navbar = () => {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -78,7 +14,7 @@ const Navbar = () => {
     };
 
     const handlePopoverClose = () => {
-        // setAnchorEl(null)
+        setAnchorEl(null)
     };
 
     const open = Boolean(anchorEl);
@@ -88,13 +24,17 @@ const Navbar = () => {
             <Box sx={{ flexGrow: 1 }}>
                 <MyAppBar position="static">
                     <Toolbar sx={{ py: 3, }}>
-                        <IconButton
+                        <MyIconButton
                             color="inherit"
-                            sx={{ mr: 2 }}
-                            onClick={() => setMenuOpen(true)}
+                            sx={{ mr: 2, width: '25px', height: '15px' }}
+                            onClick={() => {
+                                setMenuOpen(true)
+                            }}
                         >
-                            <GiHamburgerMenu />
-                        </IconButton>
+                            <span className={menuOpen ? 'animate burgerSpan' : 'burgerSpan'}></span>
+                            <span className={menuOpen ? 'animate burgerSpan' : 'burgerSpan'}></span>
+                            <span className={menuOpen ? 'animate burgerSpan' : 'burgerSpan'}></span>
+                        </MyIconButton>
                         <Drawer
                             open={menuOpen}
                             onClose={() => setMenuOpen(false)}
@@ -103,13 +43,13 @@ const Navbar = () => {
                                 sx={{ width: 250, py: 2, }}
                                 role="presentation"
                             >
-                                <IconButton
-                                    color="inherit"
-                                    sx={{ mx: 2, mb: 5 }}
+                                <MyIconButton
+                                    sx={{ m: 2, mb: 5 }}
                                     onClick={() => setMenuOpen(false)}
                                 >
-                                    <AiOutlineClose />
-                                </IconButton>
+                                    <span className={menuOpen ? 'animate closeSpan' : 'closeSpan'}></span>
+                                    <span className={menuOpen ? 'animate closeSpan' : 'closeSpan'}></span>
+                                </MyIconButton>
                                 <List sx={{ mt: 5 }}>
                                     {['home', 'products', 'lookbook', 'blog', 'about us', 'contact'].map((text) => (
                                         <MyListItem key={text} disablePadding>
@@ -124,21 +64,19 @@ const Navbar = () => {
                         <Link to="/" style={{ margin: 'auto' }}>
                             <Image src="https://template.hasthemes.com/hurst-v1/hurst/img/logo/logo.png" alt="logo" />
                         </Link>
-                        <IconButton
+                        <RightIconButton
                             size="medium"
                             edge="start"
-                            color="inherit"
                             aria-label="cart"
                             sx={{ ml: 2 }}
                             onMouseEnter={handlePopoverOpen}
                             onMouseLeave={handlePopoverClose}
                         >
-                            <Badge badgeContent={4} color="secondary">
+                            <Badge badgeContent={4}>
                                 <FaShoppingCart />
                             </Badge>
-                        </IconButton>
+                        </RightIconButton>
                         <Popover
-                            id="mouse-over-popover"
                             sx={{
                                 pointerEvents: 'none',
                                 mt: 4,
@@ -154,7 +92,6 @@ const Navbar = () => {
                                 horizontal: 'right',
                             }}
                             onClose={handlePopoverClose}
-                            disableRestoreFocus
                         >
                             <Box width={500} sx={{ py: 3, px: 2 }}>
                                 <Typography
@@ -173,14 +110,28 @@ const Navbar = () => {
                                             <Link to="/">dummy product name</Link>
                                             <p>Price: $ 100.00</p>
                                             <p>Qty: 02 </p>
-                                            <IconButton
-                                                color="inherit"
-                                            >
-                                                <AiOutlineClose />
-                                            </IconButton>
+                                            <CloseIconButton>
+                                                <AiOutlineClose color='#ccc' />
+                                            </CloseIconButton>
+                                        </ProductInfo>
+                                    </SingleProduct>
+                                    <SingleProduct>
+                                        <ImageContainer>
+                                            <Link to='/'>
+                                                <img src="https://template.hasthemes.com/hurst-v1/hurst/img/cart/1.jpg" alt="product" />
+                                            </Link>
+                                        </ImageContainer>
+                                        <ProductInfo>
+                                            <Link to="/">dummy product name</Link>
+                                            <p>Price: $ 100.00</p>
+                                            <p>Qty: 02 </p>
+                                            <CloseIconButton>
+                                                <AiOutlineClose color='#ccc' />
+                                            </CloseIconButton>
                                         </ProductInfo>
                                     </SingleProduct>
                                 </AllProducts>
+                                <Total>total: ${"500"}</Total>
                             </Box>
                         </Popover>
                     </Toolbar>
