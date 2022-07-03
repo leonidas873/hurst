c38a38d3f569dc867a9f0c4ff2fe6fe93faa27a2
leonidas873 <63054981+leonidas873@users.noreply.github.com>
2022-07-04 00:23:10 +0400

    Delete src/pages directory

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Cart from './Cart/Cart';
-import Home from './Home/Home';
-import AboutUs from './AboutUs/AboutUs';
-import ErrorPage from './ErrorPage/ErrorPage';
-import Shop from './Shop/Shop';
-import BlogDetails from './BlogDetails/BlogDetails';
-
-
-export {Cart, Home, AboutUs, ErrorPage, Shop, BlogDetails};
